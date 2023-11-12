fc6cdb8f0ece90f09b664ac0aa9ec6280e3818fe
André Fagerlie Aronsen <andre.aronsen@gmail.com>
2023-11-12 08:06:59 +0100

    Exercise 1-4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+import { createPortal } from "react-dom";
 
 function App() {
   return (
     <>
       <CharacterEditor />
       <Footer />
+      {createPortal(<div className="bottomBar" />, document.body)}
     </>
   );
 }
